dbbd75436ca55709b4c9a9d2568574a0ae31f99b
Rodrigo Racanicci <rracanicci@microsoft.com>
2023-05-11 11:07:25 -0300

    removing unused code for sensitive string

--- dotnet/src/SemanticKernel.Abstractions/Security/SensitiveString.cs
+++ dotnet/src/SemanticKernel.Abstractions/Security/SensitiveString.cs
@@ -27,11 +27,6 @@ public class SensitiveString
     public bool IsTrusted { get; }
 
     /// <summary>
-    /// The length of the wrapped string.
-    /// </summary>
-    public int Length => this.Value.Length;
-
-    /// <summary>
     /// Create a new sensitive string.
     /// </summary>
     /// <param name="value">The raw string value</param>
@@ -40,36 +35,6 @@ public class SensitiveString
     {
         this.Value = value;
         this.IsTrusted = isTrusted;
-    }
-
-    /// <summary>
-    /// Creates a copy of the string value with trusted content.
-    /// This only updates the internal trust flag.
-    /// </summary>
-    /// <returns>The new sensitive string with trusted content</returns>
-    public SensitiveString ToTrusted()
-    {
-        return new SensitiveString(this.Value, true);
-    }
-
-    /// <summary>
-    /// Creates a copy of the string value with untrusted content.
-    /// This only updates the internal trust flag.
-    /// </summary>
-    /// <returns>The new sensitive string with untrusted content</returns>
-    public SensitiveString ToUntrusted()
-    {
-        return new SensitiveString(this.Value, false);
-    }
-
-    /// <summary>
-    /// Creates a copy of the string, keeping the trust flag but updating the string value.
-    /// </summary>
-    /// <param name="value">New string value</param>
-    /// <returns>The new updated sensitive string</returns>
-    public SensitiveString UpdateValue(string value)
-    {
-        return new SensitiveString(value, this.IsTrusted);
     }
 
     /// <summary>
@@ -82,47 +47,8 @@ public class SensitiveString
         return new SensitiveString(this.Value, isTrusted);
     }
 
-    #region StringOperations
-
-    public SensitiveString Trim()
-    {
-        return new SensitiveString(this.Value.Trim(), this.IsTrusted);
-    }
-
-    public SensitiveString[] Split(params char[] separator)
-    {
-        return this.Value.Split(separator).Select(s => new SensitiveString(s, this.IsTrusted)).ToArray();
-    }
-
-    public SensitiveString Substring(int startIndex, int length)
-    {
-        return new SensitiveString(this.Value.Substring(startIndex, length), this.IsTrusted);
-    }
-
-    public SensitiveString Substring(int startIndex)
-    {
-        return new SensitiveString(this.Value.Substring(startIndex), this.IsTrusted);
-    }
-
-    public bool IsMatch(string pattern)
-    {
-        return Regex.IsMatch(this.Value, pattern);
-    }
-
-    public char this[int index]
-    {
-        get => this.Value[index];
-    }
-
     public override string ToString()
     {
         return this.Value;
     }
-
-    public static bool IsNullOrEmpty(SensitiveString? str)
-    {
-        return string.IsNullOrEmpty(str?.Value);
-    }
-
-    #endregion
 }
