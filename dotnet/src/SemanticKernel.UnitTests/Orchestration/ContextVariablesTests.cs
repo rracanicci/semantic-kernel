c614a904d28aa40633ce7a3cb62cb82ca3245cb4
Rodrigo Racanicci <rracanicci@microsoft.com>
2023-05-17 17:12:11 -0300

    apply code formatting

--- dotnet/src/SemanticKernel.UnitTests/Orchestration/ContextVariablesTests.cs
+++ dotnet/src/SemanticKernel.UnitTests/Orchestration/ContextVariablesTests.cs
@@ -4,7 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.SemanticKernel.Orchestration;
-using Microsoft.SemanticKernel.Security;
 using Xunit;
 
 namespace SemanticKernel.UnitTests.Orchestration;
